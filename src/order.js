c6a67564ccd75ac6b2c0985e6baa734ff087d185
Kelly Barry <kleemuir@gmail.com>
2022-10-15 15:43:36 -0600

    Added work refundOrder

--- src/order.js
+++ src/order.js
@@ -1,7 +1,21 @@
+function takeOrder(order, deliveryOrders) {
+  if(deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(order, deliveryOrders) {
+  for(var i = 0; i < deliveryOrders.length; i ++) {
+    if(deliveryOrders[i].orderNumber === order) {
+      //console.log(deliveryOrders[i].orderNumber);
+      deliveryOrders.slice(order);
+    }
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
